25ec75f406b9c9cdd7f46f8a6c377a2f56b844fa
Diego Sandoval <16181660+sandov@users.noreply.github.com>
2024-04-14 20:52:27 -0400

    first attempt at creating Feature model
    `bin/rails generate model Feature extern_id:string magnitude:decimal place:string time:string tsunami:boolean mag_type:string coord_long:decimal coord_lat:decimal external_url:string`

---
+++ db/migrate/20240415004957_create_features.rb
@@ -0,0 +1,17 @@
+class CreateFeatures < ActiveRecord::Migration[7.1]
+  def change
+    create_table :features do |t|
+      t.string :extern_id
+      t.decimal :magnitude
+      t.string :place
+      t.string :time
+      t.boolean :tsunami
+      t.string :mag_type
+      t.decimal :coord_long
+      t.decimal :coord_lat
+      t.string :external_url
+
+      t.timestamps
+    end
+  end
+end
