79ee56fa13e01cb5776df8b9d1ae9e4d1305e5aa
Diego Sandoval <16181660+sandov@users.noreply.github.com>
2024-04-15 11:47:28 -0400

    Comments: validate and persist working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
+
     feature = Feature.find(params[:feature_id])
+
     comment = feature.comments.build(comment_params)
+
     if comment.save
       render json: comment, status: :created
     else
@@ -12,6 +15,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:body)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   get '/api/features', to: 'features#index'
 
-  post '/features/:feature_id/comments', to: 'comments#create'
+  post 'api/features/:feature_id/comments', to: 'comments#create'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "up" => "rails/health#show", as: :rails_health_check
