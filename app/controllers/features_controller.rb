dc8f93064322a3ca033ab507cbdc000666a22446
Diego Sandoval <16181660+sandov@users.noreply.github.com>
2024-04-15 03:31:28 -0400

    get /api/features: Filters and pagination working
    A refactor would be nice

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,22 +1,42 @@
 class FeaturesController < ApplicationController
   def index
-    page_param = params[:page]
-    per_page_param = params[:per_page]
-    mag_type_param = params[:mag_type]
 
-    if mag_type_param.present?
-      records = Feature.where(mag_type: mag_type_param)
-    else
-      records = Feature.all
+    page = params.fetch(:page, 1).to_i
+    per_page = params.fetch(:per_page, 10).to_i
+    mag_type_string = params.fetch(:mag_type, "")
+
+    offset = (page - 1) * per_page;
+
+    unless mag_type_string.present?     # gets executed if mag_type_string is empty string
+      records = Feature.limit(per_page).offset(offset)
+      total = records.except(:offset, :limit, :order).count
+      records_formatted = format_for_view(records)
+      records_formatted = format_for_view(records, page, per_page, total)
+      render json: records_formatted
+      return
     end
 
-    records_formatted = format_for_view(records)
+    # if mag_type_string is not empty:
+
+    mag_type_arr = mag_type_string.split(",")
+
+    conditions = mag_type_arr.map { |mag_type| {mag_type: mag_type}}
+
+    queries = conditions.map { |condition| Feature.where(condition) }
+    records = queries.reduce(:or).limit(per_page).offset(offset)
+
+    total = records.except(:offset, :limit, :order).count
+
+    records_formatted = format_for_view(records, page, per_page, total)
     render json: records_formatted
+
+    # TODO: refactor
+
   end
 
   private
 
-  def format_for_view(raw_features)
+  def format_for_view(raw_features, page, per_page, total)
     hash_array_formatted = raw_features.map do |item|
       {
         "id" => item["id"],
@@ -43,9 +63,9 @@ class FeaturesController < ApplicationController
     parent_hash = {
       "data": hash_array_formatted,
       "pagination" => {
-        "current_page" => 1,
-        "total" => 1,
-        "per_page" => 1
+        "current_page" => page,
+        "total" => total,
+        "per_page" => per_page
       }
     }
 
