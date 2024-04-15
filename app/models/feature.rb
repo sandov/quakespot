e6300d90f72c53efe6deb8e146d991ea7068c981
Diego Sandoval <16181660+sandov@users.noreply.github.com>
2024-04-14 21:18:41 -0400

    Feature model: add validations

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,11 @@
 class Feature < ApplicationRecord
+    validates_uniqueness_of :extern_id
+
+    validates :title, presence: true
+    validates :external_url, presence: true
+    validates :place, presence: true
+
+    validates :mag_type, presence: true,  numericality: { greater_than: -1, less_than: 10 }
+    validates :coord_long, presence: true, numericality: { greater_than: -180, less_than: 180 }
+    validates :coord_lat, presence: true, numericality: { greater_than: -90, less_than: 90 }
 end
